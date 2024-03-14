73c52ed9ab61d169b933d62eb76dfd91d32c66a3
lucas <ramosmanhaes@outlook.com>
2024-03-14 12:53:00 -0300

    Criando componentes

---
+++ app/src/main/java/br/com/lucas/jurossimples/components/CaixaDeEntrada.kt
@@ -0,0 +1,25 @@
+package br.com.lucas.jurossimples.components
+
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+
+@Composable
+fun CaixaDeEntrada(
+    value: String,
+    label: String,
+    modifier: Modifier,
+    placeHolder: String,
+    keyboardType: KeyboardType
+    ) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = {},
+        modifier = modifier,
+        label = { Text(text = label)},
+        placeholder = { Text(text = placeHolder)},
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType)
+    )}
